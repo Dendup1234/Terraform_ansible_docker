e7b4c76df4b21dbe212aae07d5bd931b1ec151b2
dendup1234 <dendup091@gmail.com>
2025-11-02 02:24:56 +0600

    Done with part 1 and part 2 of assingment

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,14 +61,4 @@ resource "local_file" "inventory_file" {
   filename = "${path.module}/../ansible/inventory.ini"
 }
 
-output "ssh_commands" {
-  value = [
-    for idx, name in local.use_names :
-    "ssh ${var.ssh_user}@localhost -p ${local.use_ports[idx]}"
-  ]
-  description = "Convenience SSH commands to each container"
-}
 
-output "inventory_path" {
-  value = local_file.inventory_file.filename
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,16 @@
 output "container_names" {
   value = [for c in docker_container.node : c.name]
+  description = "All provisioned hosts"
+}
+
+output "ssh_commands" {
+  value = [
+    for idx, name in local.use_names :
+    "ssh ${var.ssh_user}@localhost -p ${local.use_ports[idx]}"
+  ]
+  description = "Convenience SSH commands to each container"
+}
+
+output "inventory_path" {
+  value = local_file.inventory_file.filename
 }
